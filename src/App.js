e689a7ae6d204b396a4a460861c066dcca85d393
vijayharshamade <vharsha736.vh@gmail.com>
2024-03-16 22:58:46 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+    <div className="congrats-card-container">
+      <h1 className="heading">Congratulations</h1>
+      <div className="card">
+        <img
+          className="profile-image"
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+          alt="profile-img"
+        />
+        <h1 className="card-title">Vijay Harsha</h1>
+        <p1 className="card-description">
+          St Ann's College Of Pharmacy, Vizianagaram
+        </p1>
+        <img
+          className="watch-image"
+          alt="watch-img"
+          src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        />
+      </div>
     </div>
-  );
-}
+  </>
+);
 
 export default App;
